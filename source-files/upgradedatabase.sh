f19c01190a6d0c9d9eb92149723d28d0290b528b
tolomaus <niek.bartholomeus@gmail.com>
2014-03-10 12:30:27 +0100

    Created a way to upgrade an existing database

---
+++ source-files/upgradedatabase.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo DB_SERVER=${DB_PORT_5432_TCP_ADDR:-${DB_SERVER}}
+echo DB_PORT=${DB_PORT_5432_TCP_PORT:-${DB_PORT}}
+cd /usr/share/tomcat6/webapps/brpm/WEB-INF
+export BUNDLE_GEMFILE=Gemfile
+export GEM_HOME=gems
+export RAILS_ENV=production
+java -classpath "lib/*" org.jruby.Main -S rake --verbose --trace db:migrate
