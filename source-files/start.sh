cb95cc85a95effe738c0c05e19971cdaae1882a9
tolomaus <niek.bartholomeus@gmail.com>
2014-03-11 14:02:11 +0100

    Increased allowed memory usage by the tomcat process

--- source-files/start.sh
+++ source-files/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo DB_SERVER=${DB_PORT_5432_TCP_ADDR:-${DB_SERVER}} >> /etc/tomcat6/tomcat6.conf
 echo DB_PORT=${DB_PORT_5432_TCP_PORT:-${DB_PORT}} >> /etc/tomcat6/tomcat6.conf
+echo JAVA_OPTS="$JAVA_OPTS -Xms512m -Xmx2048m -XX:PermSize=512m" >> /etc/tomcat6/tomcat6.conf
+
 /etc/init.d/tomcat6 start
 tail -F /var/log/tomcat6/catalina.out
