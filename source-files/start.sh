abcf1db09adc400f3f8e4e35fbe8a73e164cce17
tolomaus <niek.bartholomeus@gmail.com>
2014-10-05 11:10:56 +0200

    Support service discovery environment variables as created by kubernetes

--- source-files/start.sh
+++ source-files/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo DB_SERVER=${DB_PORT_5432_TCP_ADDR:-${DB_SERVER}} >> /etc/tomcat6/tomcat6.conf
-echo DB_PORT=${DB_PORT_5432_TCP_PORT:-${DB_PORT}} >> /etc/tomcat6/tomcat6.conf
+echo DB_SERVER=${BRPMDBTEST_SERVICE_HOST:-${DB_PORT_5432_TCP_ADDR:-${DB_SERVER}}} >> /etc/tomcat6/tomcat6.conf
+echo DB_PORT=${BRPMDBTEST_SERVICE_PORT:-${DB_PORT_5432_TCP_PORT:-${DB_PORT}}} >> /etc/tomcat6/tomcat6.conf
 echo JAVA_OPTS=\"\$JAVA_OPTS -Xms512m -Xmx2048m -XX:PermSize=512m\" >> /etc/tomcat6/tomcat6.conf
 
 /etc/init.d/tomcat6 start
